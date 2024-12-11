8e7af10407c90d169fad04871bc84f3cd7aed79c
Emma <emmanuelfongong10@gmail.com>
2024-12-11 19:22:09 +0100

    Day 4

---
+++ Day 4/list.py
@@ -0,0 +1,14 @@
+states= ['carlifornia', 'nebraska', 'florida', 'new york', 'nevada']
+
+# print carlifornia
+print(states[0])
+
+# changing the element at index one
+states[1]= 'Arizona'
+# adding an item to the end of the list
+states.append("marryland")
+
+# adding a list to the above list
+states.extend(['lowa', 'Texas', 'Mississipi'])
+print(states) 
+
---
+++ Day 4/treasure_map.py
@@ -0,0 +1,40 @@
+row1=['A', 'A', 'A']
+row2= ['B', 'B', 'B']
+row3= ['C', 'C', 'C']
+
+map= [row1, row2, row3]
+
+
+print(f'{row1}\n{row2}\n{row3}')
+
+position= int(input('enter position'))
+
+if position==11:
+    row1[0]='X'
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==12:
+    row2[0]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==13:
+    row3[0]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==21:
+    row1[1]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==22:
+    row2[1]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==23:
+    row3[1]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==31:
+    row1[2]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==32:
+    row2[2]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+elif position==33:
+    row3[2]="X"
+    print(f'{row1}\n{row2}\n{row3}')
+else:
+    print("invalid input")
