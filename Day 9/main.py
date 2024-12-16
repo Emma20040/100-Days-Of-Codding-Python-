8a8e8715a9adc258e050765275dfbbd2ff6bfdba
Emma <emmanuelfongong10@gmail.com>
2024-12-16 16:40:32 +0100

    Day 9 completed

---
+++ Day 9/main.py
@@ -0,0 +1,11 @@
+programming_dic= {
+    "bug":"an error in a program",
+    "fuction": "a piece of code that can easily be called over and over again"
+}
+programming_dic["loop"]= "performing an action over and over"
+programming_dic["bug"]= "eror!!!!!"
+# looping through a dictionary
+for key in programming_dic:
+    print(key)
+    print(programming_dic[key])
+# print(programming_dic)
